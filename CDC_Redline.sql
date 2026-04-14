a0512cd07f7cfc5034bf16250f94109f52018acc
Abe-E <abeepstein@gmail.com>
2026-04-13 20:46:08 -0600

    Update CDC_Redline.sql

--- CDC_Redline.sql
+++ CDC_Redline.sql
@@ -2,6 +2,12 @@ SELECT
 * 
 FROM
  `Final Project`.CDC_Data AS CDC
+ Case # Quantifying Grades for analysis
+	WHEN Line.grade = "A" then 4 
+    WHEN Line.grade = "B" then 3
+    WHEN Line.grade = "C" then 2
+    WHEN Line.grade = "D" then 1 
+END AS "Redline Score"
  JOIN
  `Final Project`.Neighborhood_Zips AS Zips
  ON CDC.LocationName = Zips.Zip_Code
